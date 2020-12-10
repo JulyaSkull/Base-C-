674ee85bb27d2cb0b8db09ca723e52a891e31c73
Julya <jula.popova99@gmail.com>
2020-12-11 00:11:36 +0300

    OOP2

---
+++ Task_03/task_1/Employees.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    public class Employees<T> : Person 
+    {
+        private int expirience;
+        public string Post;
+        public T ID { get; set; }
+        public int Expirience
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    expirience = value;
+                }
+                else
+                {
+                    Console.WriteLine("Стаж должен быть больше 0");
+                }
+            }
+            get
+            {
+                return expirience;
+            }
+        }
+
+        public Employees(T ID, string FirstName, string LastName, string Patronymic, int Expirience, string Post)
+            : base(FirstName, LastName, Patronymic)
+        {
+            this.ID = ID;
+            this.Expirience = Expirience;
+            this.Post = Post;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($" ID работника:{ID} Имя: {FirstName}, Фамилия: {LastName}, Отчество: {Patronymic}, \n Стаж: {Expirience}, Должность: {Post}");
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Employees<T> employees &&
+                   expirience == employees.expirience &&
+                   Post == employees.Post;
+        }
+    }
+}
---
+++ Task_03/task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employees<string> emp1 = new Employees<string>("4", "Александра", "Волкова", "Анатольевна", 14, "Главный менеджер");
+            Console.WriteLine(emp1.ToString());
+            Console.WriteLine("\n****************************************************************");
+            Employees<int> emp2 = new Employees<int>(1, "Максим", "Иванов", "Евгеньевич", 11, "Бухгалтер");
+            Console.WriteLine(emp2.ToString()); 
+            Console.WriteLine("\n****************************************************************");
+            Employees<int> emp3 = new Employees<int>(3, "Крупина", "Любовь", "Сергеевна", 11, "Бухгалтер");
+            Console.WriteLine(emp3.ToString());
+            Console.WriteLine("\n****************************************************************");
+            Employees<int> emp4 = new Employees<int>(2, "Ильин", "Тимофей", "Александрович", 13, "Старший программист");
+            Console.WriteLine(emp4.ToString());
+
+            Console.WriteLine(emp2.Equals(emp3));
+            Console.ReadKey();
+        }
+    }
+}
